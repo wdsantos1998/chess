1b25d55ee30dc3557637bdb05747a0cccd64c1cc
Wesle dos Santos <wdsantoslds@gmail.com>
2025-01-24 21:30:51 -0700

    Feat: QueenMovesCalculator implemented. Base of BishopMovesCalculator used.

--- shared/src/main/java/chess/moves_calculator/QueenMovesCalculator.java
+++ shared/src/main/java/chess/moves_calculator/QueenMovesCalculator.java
@@ -2,24 +2,102 @@ package chess.moves_calculator;
 
 import chess.ChessBoard;
 import chess.ChessMove;
+import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class QueenMovesCalculator implements PieceMovesCalculator {
+
+    private boolean capturedPiece = false;
+
+    private int[][] getQueenMoves() {
+        return new int[][]{
+                {1,0},{-1,0},
+                {0,1},{0,-1},
+                {1,1},{1,-1},
+                {-1,1},{-1,-1}
+        };
+    }
+
     @Override
     public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition myPosition) {
-        return List.of();
+        int[][] queenMoves = getQueenMoves();
+        Collection<ChessMove> validMoves = new ArrayList<>();
+        for (int[] move : queenMoves) {
+            setCapturedPiece(false);
+            int currentRow = myPosition.getRow();
+            int currentCol = myPosition.getColumn();
+            while (true) {
+                {
+                    currentRow += move[0];
+                    currentCol += move[1];
+                    ChessPosition newPos = new ChessPosition(currentRow, currentCol);
+
+                    if (isValidMove(board, myPosition, newPos)) {
+                        validMoves.add(new ChessMove(myPosition, newPos, null));
+                        if (isCapturedPiece()) {
+                            break;
+                        }
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+        }
+        return validMoves;
     }
 
     @Override
     public boolean isValidMove(ChessBoard board, ChessPosition myPosition, ChessPosition targetPosition) {
-        return false;
+        if (!isWithinBoard(targetPosition)) {
+            return false;
+        }
+        ChessPiece myPiece = board.getPiece(myPosition);
+        ChessPiece targetPiece = board.getPiece(targetPosition);
+
+        if (targetPiece == null) {
+            return true;
+        }
+
+        else if (myPiece != null && targetPiece.getTeamColor() == myPiece.getTeamColor()) {
+            return false;
+        }
+        else if (myPiece != null && targetPiece.getTeamColor() != myPiece.getTeamColor()) {
+            setCapturedPiece(true);
+            return true;
+        }
+
+        return true;
+    }
+
+    private boolean isWithinBoard(ChessPosition targetPosition) {
+        return targetPosition.getRow() >= 1 && targetPosition.getRow() <= 8 && targetPosition.getColumn() >= 1 && targetPosition.getColumn() <= 8;
     }
 
     @Override
     public boolean isEnemy(ChessBoard board, ChessPosition myPosition) {
         return false;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public boolean isCapturedPiece() {
+        return capturedPiece;
+    }
+
+    public void setCapturedPiece(boolean capturedPiece) {
+        this.capturedPiece = capturedPiece;
     }
 }
