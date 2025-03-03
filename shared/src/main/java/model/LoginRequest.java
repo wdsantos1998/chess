1f954b3f1a4584a4db55998084608a3658407157
Wesle dos Santos <wdsantoslds@gmail.com>
2025-03-03 02:20:18 -0700

    Feat: Creating loginRequest data class.

---
+++ shared/src/main/java/model/LoginRequest.java
@@ -0,0 +1,20 @@
+package model;
+
+public class LoginRequest {
+    private final String username;
+    private final String password;
+
+    public LoginRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+}
