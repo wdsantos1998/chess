a21f3608f8f76d4508eb3d024f71750c8e7589c7
Wesle dos Santos <wdsantoslds@gmail.com>
2025-03-03 02:25:40 -0700

    Fix: Modifying classes to follow best POJO practices. It should avoid errors at the serializing and deserializing stages.

--- shared/src/main/java/model/User.java
+++ shared/src/main/java/model/User.java
@@ -27,32 +27,4 @@ public class User {
     public String getEmail() {
         return email;
     }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        User user = (User) o;
-        return Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(email, user.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(username, password, email);
-    }
 }
--- shared/src/main/java/model/authData.java
+++ shared/src/main/java/model/authData.java
@@ -1,6 +1,4 @@
 package model;
-
-import java.util.Objects;
 import java.util.UUID;
 
 public class authData {
@@ -17,32 +15,7 @@ public class authData {
         return authToken;
     }
 
-    public void setAuthToken(String authToken) {
-        this.authToken = authToken;
-    }
-
     public String getUsername() {
         return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        authData authData = (authData) o;
-        return Objects.equals(authToken, authData.authToken) && Objects.equals(username, authData.username);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(authToken, username);
     }
 }
