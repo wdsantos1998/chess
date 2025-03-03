7f18e320a71d21b09bd493ed4fc60537031912c0
Wesle dos Santos <wdsantoslds@gmail.com>
2025-03-03 06:11:13 -0700

    Feat: Implementing comparison for User and LoginRequest

--- shared/src/main/java/model/LoginRequest.java
+++ shared/src/main/java/model/LoginRequest.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class LoginRequest {
     private final String username;
     private final String password;
@@ -17,4 +19,20 @@ public class LoginRequest {
         return username;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LoginRequest that = (LoginRequest) o;
+        return Objects.equals(username, that.username) && Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
+    }
 }
--- shared/src/main/java/model/User.java
+++ shared/src/main/java/model/User.java
@@ -29,6 +29,23 @@ public class User {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password, email);
+    }
+
+    @Override
     public String toString() {
         return "User{" +
                 "username='" + username + '\'' +
