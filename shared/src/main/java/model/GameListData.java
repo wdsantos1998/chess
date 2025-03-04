a307b93512355213b33bd8b0a2ee8a83a3a979d0
Wesle dos Santos <wdsantoslds@gmail.com>
2025-03-03 22:45:41 -0700

    Feat: Creating GamesListData model.

---
+++ shared/src/main/java/model/GameListData.java
@@ -0,0 +1,31 @@
+package model;
+
+public class GameListData {
+    private final int gameID;
+    private final String whiteUsername;
+    private final String blackUsername;
+    private final String gameName;
+
+    public GameListData(int gameID, String whiteUsername, String blackUsername, String gameName) {
+        this.gameID = gameID;
+        this.whiteUsername = whiteUsername;
+        this.blackUsername = blackUsername;
+        this.gameName = gameName;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
+
+    public String getWhiteUsername() {
+        return whiteUsername;
+    }
+
+    public String getBlackUsername() {
+        return blackUsername;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+}
