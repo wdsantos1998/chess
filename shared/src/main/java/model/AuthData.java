69beebc20e39a8975fe36860a3a9f821739ddf52
Wesle dos Santos <wdsantoslds@gmail.com>
2025-03-05 00:40:58 -0700

    Feat: Just to commit something again.

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -6,5 +6,5 @@ public record AuthData(String username, String authToken) {
     public AuthData(String username) {
         this(username, UUID.randomUUID()+ username);
     }
-    //Just to commit something 1
+    //Just to commit something 2
 }
