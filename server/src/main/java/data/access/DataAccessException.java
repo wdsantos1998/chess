90978aefb498e8a45c293a2875fe570513e4793e
Wesle dos Santos <wdsantoslds@gmail.com>
2025-03-05 00:30:15 -0700

    Feat: Adding new files

---
+++ server/src/main/java/data/access/DataAccessException.java
@@ -0,0 +1,10 @@
+package data.access;
+
+/**
+ * Indicates there was an error connecting to the database
+ */
+public class DataAccessException extends Exception{
+    public DataAccessException(String message) {
+        super(message);
+    }
+}
---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,38 @@
+package server;
+
+import controller.AppHandler;
+import data.access.MemoryDataAccess;
+import service.AppService;
+import spark.*;
+
+import static spark.Spark.exception;
+
+public class Server {
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+
+        // Register your endpoints and handle exceptions here.
+        AppHandler userHandler = new AppHandler(new AppService(new MemoryDataAccess()));
+        userHandler.startRoutes();
+
+        // Handle unhandled exceptions
+        exception(Exception.class, (e, req, res) -> {
+            res.status(500);
+            res.body("Internal Server Error: " + e.getMessage());
+        });
+
+        //This line initializes the server and can be removed once you have a functioning endpoint 
+        Spark.init();
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+}
