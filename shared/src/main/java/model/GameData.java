95205676796020812efe532b88f832786e1d4b5e
Wesle dos Santos <wdsantoslds@gmail.com>
2025-03-05 13:09:36 -0700

    Fix: Removing nullable package.

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -1,12 +1,11 @@
 package model;
 
 import chess.ChessGame;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Random;
 
-public record GameData(@Nullable String whiteUsername, @Nullable String blackUsername, String gameName, int gameID, ChessGame game) {
-    public GameData(@Nullable String whiteUsername, @Nullable String blackUsername, String gameName) {
+public record GameData( String whiteUsername, String blackUsername, String gameName, int gameID, ChessGame game) {
+    public GameData( String whiteUsername, String blackUsername, String gameName) {
         this(whiteUsername, blackUsername, gameName,  Math.abs(new Random().nextInt()), new ChessGame());
     }
 
