90691bd4653438768e71da53c3d8752592503774
Wesle dos Santos <wdsantoslds@gmail.com>
2025-03-12 16:23:57 -0600

    Feat: Refactoring AuthData.

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -4,7 +4,6 @@ import java.util.UUID;
 
 public record AuthData(String username, String authToken) {
     public AuthData(String username) {
-        this(username, UUID.randomUUID()+ username);
+        this(username, UUID.randomUUID().toString() + username);
     }
-    //Just to commit something 3
 }
