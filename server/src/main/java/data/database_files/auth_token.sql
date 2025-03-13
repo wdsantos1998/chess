38b8dc5dcbd28f76ef013b77eeffac0ce08f5dad
Wesle dos Santos <wdsantoslds@gmail.com>
2025-03-12 20:38:31 -0600

    Fix: Making username comparison case-insensitive and password comparison case-sensitive.

--- server/src/main/java/data/database_files/auth_token.sql
+++ server/src/main/java/data/database_files/auth_token.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS auth_token (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    authToken VARCHAR(100),
-    username VARCHAR(100)
+    authToken VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci,
+    username VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci
     );
--- server/src/main/java/data/database_files/users.sql
+++ server/src/main/java/data/database_files/users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100),
-    password VARCHAR(100),
+    username VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci,
+    password VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_bin,
     email VARCHAR(100)
     );
