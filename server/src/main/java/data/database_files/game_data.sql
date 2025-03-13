00000b0f055c01860c87adca86358918baede1e9
Wesle dos Santos <wdsantoslds@gmail.com>
2025-03-12 18:41:01 -0600

    Fix: whiteUsername and blackUsername may accept null values.

--- server/src/main/java/data/database_files/game_data.sql
+++ server/src/main/java/data/database_files/game_data.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS game_data (
     id INT AUTO_INCREMENT PRIMARY KEY,
     gameID INT UNIQUE,
-    whiteUsername VARCHAR(100),
-    blackUsername VARCHAR(100),
+    whiteUsername VARCHAR(100) NULL,
+    blackUsername VARCHAR(100) NULL,
     gameName VARCHAR(100),
     game JSON
     );
