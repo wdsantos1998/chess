1f908331e0592000de83d278f21488d83b077d93
Wesle dos Santos <wdsantoslds@gmail.com>
2025-03-27 17:27:00 -0600

    Feat: Listing games sequentially.

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,16 +1,23 @@
 package ui;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.google.gson.Gson;
 import model.*;
+import org.glassfish.grizzly.utils.Exceptions;
 
 
 public class ChessClient {
     private final ServerFacade server;
     private AuthData userToken;
+    private Map <Integer, Integer> gameMap;
 
     public ChessClient(String ServerUrl) {
         server = new ServerFacade(ServerUrl);
+        gameMap = new HashMap<>();
     }
 
     public boolean login(LoginRequest loginRequest) throws Exception {
@@ -36,9 +43,9 @@ public class ChessClient {
 
     public List<GameListData> listGames() throws Exception {
         try {
-            return server.listGames(userToken.authToken());
+            return turnGameListIntoSequenceOfIndexes(server.listGames(userToken.authToken()));
         } catch (Exception e) {
-           throw new Exception("Failed to list games");
+            throw new Exception("Failed to list games");
         }
     }
 
@@ -53,7 +60,7 @@ public class ChessClient {
 
     public GameData createGame(String gameName) throws Exception {
         try {
-            return server.createGame(gameName, userToken.authToken());
+            return addToGameIndexList(server.createGame(gameName, userToken.authToken()));
         } catch (Exception e) {
             throw new Exception("Failed to create game");
         }
@@ -61,7 +68,7 @@ public class ChessClient {
 
     public void joinGame(int gameID, String playerColor) throws Exception {
         try {
-            server.joinGame(new JoinGameRequest(userToken.authToken(), playerColor, gameID));
+            server.joinGame(new JoinGameRequest(userToken.authToken(), playerColor, gameMap.get(gameID)));
         } catch (Exception e) {
             throw new Exception("Failed to join game");
         }
@@ -72,5 +79,31 @@ public class ChessClient {
             return false;
         }
         return true;
+    }
+
+    private List<GameListData> turnGameListIntoSequenceOfIndexes(List<GameListData> gameList) {
+        List<GameListData> returnGameList = new ArrayList<>();
+        gameList.forEach(game -> {
+            int mapLength = this.gameMap.size();
+            if(mapLength == 0){
+                int currentIndexOfGame = mapLength+1;
+                this.gameMap.put(currentIndexOfGame, game.gameID());
+                returnGameList.add(new GameListData(currentIndexOfGame, game.whiteUsername(), game.blackUsername(), game.gameName()));
+            }
+            else{
+                if(!this.gameMap.containsValue(game.gameID())){
+                    int currentIndexOfGame = mapLength+1;
+                    this.gameMap.put(currentIndexOfGame, game.gameID());
+                    returnGameList.add(new GameListData(currentIndexOfGame, game.whiteUsername(), game.blackUsername(), game.gameName()));
+                }
+            }
+        });
+        return returnGameList;
+    }
+
+    private GameData addToGameIndexList(GameData gameData) {
+        int gameID = gameMap.size() + 1;
+        gameMap.put(gameID, gameData.gameID());
+        return new GameData(gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName(), gameID, gameData.game());
     }
 }
