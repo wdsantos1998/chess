3f825b6b174a466bbaf8027c7f381faa331abe8a
Wesle dos Santos <wdsantoslds@gmail.com>
2025-03-27 18:49:14 -0600

    Feat: Personalized errors and fixing join game problem.

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -5,13 +5,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import model.*;
 
 
 public class ChessClient {
     private final ServerFacade server;
     private AuthData userToken;
-    private Map <Integer, Integer> gameMap;
+    private Map<Integer, Integer> gameMap;
 
     public ChessClient(String ServerUrl) {
         server = new ServerFacade(ServerUrl);
@@ -22,9 +24,8 @@ public class ChessClient {
         try {
             this.userToken = server.login(loginRequest);
             return this.userToken != null && this.userToken.authToken() != null;
-        }
-        catch (Exception e) {
-            throw new Exception("Failed to login");
+        } catch (ExceptionResponse e) {
+            throw new Exception(handleException(e.getMessage()));
         }
 
     }
@@ -33,9 +34,8 @@ public class ChessClient {
         try {
             this.userToken = server.register(userData);
             return this.userToken != null && this.userToken.authToken() != null;
-        }
-        catch (Exception e) {
-            throw new Exception("Failed to register");
+        } catch (Exception e) {
+            throw new Exception(handleException(e.getMessage()));
         }
     }
 
@@ -43,7 +43,7 @@ public class ChessClient {
         try {
             return turnGameListIntoSequenceOfIndexes(server.listGames(userToken.authToken()));
         } catch (Exception e) {
-            throw new Exception("Failed to list games");
+            throw new Exception(handleException(e.getMessage()));
         }
     }
 
@@ -52,7 +52,7 @@ public class ChessClient {
             server.logout(userToken.authToken());
             userToken = null;
         } catch (Exception e) {
-            throw new Exception("Failed to logout");
+            throw new Exception(handleException(e.getMessage()));
         }
     }
 
@@ -60,19 +60,23 @@ public class ChessClient {
         try {
             return addToGameIndexList(server.createGame(gameName, userToken.authToken()));
         } catch (Exception e) {
-            throw new Exception("Failed to create game");
+            throw new Exception(handleException(e.getMessage()));
         }
     }
 
     public void joinGame(int gameID, String playerColor) throws Exception {
         try {
+            turnGameListIntoSequenceOfIndexes(server.listGames(userToken.authToken()));
+            if(!gameMap.containsKey(gameID)){
+                throw new Exception("Error: gameID not found");
+            }
             server.joinGame(new JoinGameRequest(userToken.authToken(), playerColor, gameMap.get(gameID)));
         } catch (Exception e) {
-            throw new Exception("Failed to join game");
+            throw new Exception(handleException(e.getMessage()));
         }
     }
 
-    public boolean isClientLoggedIn(){
+    public boolean isClientLoggedIn() {
         if (userToken == null || userToken.authToken() == null) {
             return false;
         }
@@ -83,14 +87,13 @@ public class ChessClient {
         List<GameListData> returnGameList = new ArrayList<>();
         gameList.forEach(game -> {
             int mapLength = this.gameMap.size();
-            if(mapLength == 0){
-                int currentIndexOfGame = mapLength+1;
+            if (mapLength == 0) {
+                int currentIndexOfGame = mapLength + 1;
                 this.gameMap.put(currentIndexOfGame, game.gameID());
                 returnGameList.add(new GameListData(currentIndexOfGame, game.whiteUsername(), game.blackUsername(), game.gameName()));
-            }
-            else{
-                if(!this.gameMap.containsValue(game.gameID())){
-                    int currentIndexOfGame = mapLength+1;
+            } else {
+                if (!this.gameMap.containsValue(game.gameID())) {
+                    int currentIndexOfGame = mapLength + 1;
                     this.gameMap.put(currentIndexOfGame, game.gameID());
                     returnGameList.add(new GameListData(currentIndexOfGame, game.whiteUsername(), game.blackUsername(), game.gameName()));
                 }
@@ -103,5 +106,15 @@ public class ChessClient {
         int gameID = gameMap.size() + 1;
         gameMap.put(gameID, gameData.gameID());
         return new GameData(gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName(), gameID, gameData.game());
+    }
+
+    private String handleException(String e) {
+        try {
+            JsonObject obj = JsonParser.parseString(e).getAsJsonObject();
+            return obj.get("message").getAsString();
+        } catch (Exception parseEx) {
+            //If the exception is not a JSON object, then it is a string
+            return e;
+        }
     }
 }
