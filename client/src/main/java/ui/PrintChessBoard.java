2c725241f9b120b5f4ae1c07db4a7e36a391ac8b
Wesle dos Santos <wdsantoslds@gmail.com>
2025-03-27 18:35:54 -0600

    Feat: Printing row and column indexes.

--- client/src/main/java/ui/PrintChessBoard.java
+++ client/src/main/java/ui/PrintChessBoard.java
@@ -1,13 +1,17 @@
 package ui;
+
 import chess.ChessGame;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import ui.EscapeSequences;
 import chess.ChessBoard;
+
 public class PrintChessBoard {
+    private static final int BOARD_SIZE = 8;
+
     public static void printGenericBoard(String color) {
-        if(color.equals("black")) {
-            System.out.println("Printing a generic chessboard from the perspective of the " + color + " player");
+        if (color.equals("black")) {
+            System.out.println("Displaying the chessboard from the perspective of the " + color + " player");
             //Drawing board from the perspective of the black player
             String[][] board = {
                     // row 0 - black main pieces
@@ -41,11 +45,10 @@ public class PrintChessBoard {
                             EscapeSequences.WHITE_KING, EscapeSequences.WHITE_BISHOP, EscapeSequences.WHITE_KNIGHT, EscapeSequences.WHITE_ROOK}
             };
 
-            printBoard(board);
-        }
-        else {
+            printBoard(board, true);
+        } else {
 
-            System.out.println("Printing a generic chessboard from the perspective of the " + color + " player");
+            System.out.println("Displaying the chessboard from the perspective of the " + color + " player");
 
             //Drawing board from the perspective of the white player
             String[][] board = {
@@ -76,17 +79,28 @@ public class PrintChessBoard {
 
 
                     // row 7  black main pieces
-                    {EscapeSequences.BLACK_ROOK, EscapeSequences.BLACK_KNIGHT, EscapeSequences.BLACK_BISHOP,  EscapeSequences.BLACK_KING, EscapeSequences.BLACK_QUEEN,
+                    {EscapeSequences.BLACK_ROOK, EscapeSequences.BLACK_KNIGHT, EscapeSequences.BLACK_BISHOP, EscapeSequences.BLACK_KING, EscapeSequences.BLACK_QUEEN,
                             EscapeSequences.BLACK_BISHOP, EscapeSequences.BLACK_KNIGHT, EscapeSequences.BLACK_ROOK},
             };
-
-            printBoard(board);
+            printBoard(board, false);
         }
     }
 
-    private static void printBoard(String[][] board) {
-        for (int row = 0; row < 8; row++) {
-            for (int col = 0; col < 8; col++) {
+    private static void printBoard(String[][] board, boolean reverse) {
+        System.out.print(EscapeSequences.RESET_TEXT_COLOR + "   ");
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            char c = (char) (reverse ? 'H' - i : 'A' + i);
+            System.out.print(" " + c + " "); // 3 boxes wide column to align with the pieces
+        }
+        System.out.println();
+
+        for (int r = 0; r < BOARD_SIZE; r++) {
+            int row = reverse ? r : BOARD_SIZE - 1 - r;
+            System.out.print(EscapeSequences.RESET_TEXT_COLOR + " " + (row + 1) + " ");
+
+            for (int c = 0; c < BOARD_SIZE; c++) {
+                int col = reverse ? BOARD_SIZE - 1 - c : c;
+
                 if ((row + col) % 2 == 0) {
                     System.out.print(EscapeSequences.SET_BG_COLOR_LIGHT_GREY);
                 } else {
@@ -94,30 +108,17 @@ public class PrintChessBoard {
                 }
 
                 System.out.print(board[row][col]);
-
                 System.out.print(EscapeSequences.RESET_BG_COLOR);
             }
-            System.out.println();
+
+            System.out.println(EscapeSequences.RESET_TEXT_COLOR + " " + (row + 1));
+        }
+
+        System.out.print(EscapeSequences.RESET_TEXT_COLOR + "   ");
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            char c = (char) (reverse ? 'H' - i : 'A' + i);
+            System.out.print(" " + c + " "); // Wide column to align with the pieces
         }
         System.out.println();
     }
-
-    private static String getPieceRepresentation(ChessPiece.PieceType type, boolean isWhite) {
-            switch (type) {
-                case KING:
-                    return isWhite ? EscapeSequences.WHITE_KING : EscapeSequences.BLACK_KING;
-                case QUEEN:
-                    return isWhite ? EscapeSequences.WHITE_QUEEN : EscapeSequences.BLACK_QUEEN;
-                case BISHOP:
-                    return isWhite ? EscapeSequences.WHITE_BISHOP : EscapeSequences.BLACK_BISHOP;
-                case KNIGHT:
-                    return isWhite ? EscapeSequences.WHITE_KNIGHT : EscapeSequences.BLACK_KNIGHT;
-                case ROOK:
-                    return isWhite ? EscapeSequences.WHITE_ROOK : EscapeSequences.BLACK_ROOK;
-                case PAWN:
-                    return isWhite ? EscapeSequences.WHITE_PAWN : EscapeSequences.BLACK_PAWN;
-                default:
-                    return EscapeSequences.EMPTY;
-            }
-        }
-    }
+}
