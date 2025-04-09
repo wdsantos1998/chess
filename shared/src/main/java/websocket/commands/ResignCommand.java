c9f160e2a4b2bddaf714d99085d5fa86effb628d
Wesle dos Santos <wdsantoslds@gmail.com>
2025-04-09 12:31:00 -0600

    Feat: Implementing ResignCommand.

---
+++ shared/src/main/java/websocket/commands/ResignCommand.java
@@ -0,0 +1,8 @@
+package websocket.commands;
+
+public class ResignCommand extends UserGameCommand {
+
+    public ResignCommand(String authToken, Integer gameID) {
+        super(CommandType.RESIGN, authToken, gameID);
+    }
+}
