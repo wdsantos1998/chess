e2be449b5398cba6d3987e91f98ce320e89566a5
Wesle dos Santos <wdsantoslds@gmail.com>
2025-04-09 12:13:01 -0600

    Feat: Implementing MakeMoveCommand and NotificationHandler.java

---
+++ client/src/main/java/websocket/NotificationHandler.java
@@ -0,0 +1,7 @@
+package websocket;
+
+import websocket.messages.Notification;
+
+public interface NotificationHandler {
+    void notify(Notification notification);
+}
---
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -0,0 +1,16 @@
+package websocket.commands;
+
+import chess.ChessMove;
+
+public class MakeMoveCommand extends UserGameCommand {
+    private final ChessMove move;
+
+    public MakeMoveCommand(CommandType commandType, String authToken, Integer gameID, ChessMove move) {
+        super(commandType, authToken, gameID);
+        this.move = move;
+    }
+
+    public ChessMove getMove() {
+        return move;
+    }
+}
