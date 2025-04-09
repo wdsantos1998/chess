caeb220ea5ca02ff662590e06f72fd9f0c547496
Wesle dos Santos <wdsantoslds@gmail.com>
2025-04-09 12:35:39 -0600

    Feat: Implementing ConnectionManager

---
+++ server/src/main/java/server/ConnectionManager.java
@@ -0,0 +1,32 @@
+package server;
+
+import com.google.gson.Gson;
+import websocket.messages.Notification;
+
+import javax.websocket.Session;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    private final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+
+    public void addConnection(String authToken, int gameID, Session session) {
+        connections.put(authToken, new Connection(authToken, gameID, session));
+    }
+
+    public void removeConnections(String authToken) {
+        connections.remove(authToken);
+    }
+
+    public void broadcast(String excludeAuthToken, Integer gameID, Notification notification) throws Exception {
+        var removeList = new ArrayList<Connection>();
+        for (var c : connections.values()) {
+            if (c.session.isOpen() && !c.authToken.equals(excludeAuthToken) && Objects.equals(c.gameID, gameID)) {
+                c.sendMessage(new Gson().toJson(notification));
+            }
+
+        }
+    }
+}
