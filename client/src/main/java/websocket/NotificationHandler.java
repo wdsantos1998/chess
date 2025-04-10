7a2fe9883d48bef3d8ac2d7a2d6e0c22b9fb8b85
Wesle dos Santos <wdsantoslds@gmail.com>
2025-04-10 14:13:08 -0600

    Feat: Notification handler.

--- client/src/main/java/websocket/NotificationHandler.java
+++ client/src/main/java/websocket/NotificationHandler.java
@@ -1,7 +1,12 @@
 package websocket;
 
+import websocket.messages.LoadGame;
 import websocket.messages.Notification;
 
 public interface NotificationHandler {
     void notify(Notification notification);
+
+    void load(LoadGame loadGame);
+
+    void warn(Error error);
 }
