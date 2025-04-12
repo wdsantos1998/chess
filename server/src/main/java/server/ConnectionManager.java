1c9010ccba6ec00828eaff5396bc675d28193756
Wesle dos Santos <wdsantoslds@gmail.com>
2025-04-12 12:33:47 -0600

    Fix: Guarantee that information is being sent to the right game.

--- server/src/main/java/server/ConnectionManager.java
+++ server/src/main/java/server/ConnectionManager.java
@@ -21,10 +21,11 @@ public class ConnectionManager {
 
     public void broadcast(String excludeAuthToken, Integer gameID, Notification notification) throws Exception {
         if (connections.isEmpty()) {
-            System.out.println("No sessions found for token " + excludeAuthToken);
+            System.out.println("No sessions found for token");
             return;
         }
         for (var c : connections.values()) {
+            //I can guarantee that the session is open, because I check it in the addConnection method
             if (c != null && c.session.isOpen() && !c.authToken.equals(excludeAuthToken) && Objects.equals(c.gameID, gameID)) {
                 System.out.println("Sending to user: " + c.authToken);
                 c.sendMessage(gson.toJson(notification));
